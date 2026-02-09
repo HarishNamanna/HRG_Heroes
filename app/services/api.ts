fa1e08b8d704510db5f9532e37eb5e7e1e96af81
HarishN1802 <harishnamann@shriramsoftwares.com>
2026-02-09 13:59:35 +0530

    genarlising IP

--- app/services/api.ts
+++ app/services/api.ts
@@ -2,7 +2,7 @@
  import axios from "axios";
 
 // CHANGE THIS to your laptop IP
-const BASE_URL = "http://192.168.31.253:3000/api";
+const BASE_URL = "http://192.168.XX.XXX:3000/api";
 
 export const api = axios.create({
   baseURL: BASE_URL,
